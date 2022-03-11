91ef4aebcc99082be1faf5d095a4ccd0c40f1274
chenyehong <ye_linux@126.com>
2022-03-11 10:45:04 +0800

    Initial Commit

---
+++ UIKitPreviewNote/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  UIKitPreviewNote
+//
+//  Created by chenyehong on 2022/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UIKitPreviewNote/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UIKitPreviewNote
+//
+//  Created by chenyehong on 2022/3/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
